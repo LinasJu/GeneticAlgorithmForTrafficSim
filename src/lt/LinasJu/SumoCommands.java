1c6d34ba319382aa3269ec8a417b696fc8264eb9
LinasJu <linas.jurgelenas@gmail.com>
2020-04-26 12:07:01 +0300

    kabliataskio vieta

--- src/lt/LinasJu/SumoCommands.java
+++ src/lt/LinasJu/SumoCommands.java
@@ -6,8 +6,8 @@ public enum SumoCommands {
     NETCONVERT ("netconvert"),
     COMMAND_ROAD_NETWORK_INPUT ("--net-file %S"), // tinklas sukurtas su NETCONVERT arba NETGENERATE
 
-    NETWORK_OUTPUT_FILE_NAME("-o %S.net.xml")
-    ;
+    NETWORK_OUTPUT_FILE_NAME("-o %S.net.xml");
+
     private final String command;
 
     SumoCommands(final String command) {
